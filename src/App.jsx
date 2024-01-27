e0f2cb788ce1f4e9bb16dfcd97b806a6cd33e6e8
ahmadullahmukhlis <nasarimukhlis@gmail.com>
2024-01-27 08:56:54 -0800

    the route added

--- src/App.jsx
+++ src/App.jsx
@@ -5,12 +5,21 @@ import "./vendors/css/vendor.bundle.base.css";
 import "./vendors/daterangepicker/daterangepicker.css";
 import "./vendors/chartist/chartist.min.css";
 import "./css/style.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Header from './Componants/Header.jsx';
 
 function App() {
   return (
     <>
-      <Header/>
+      <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Header/>}>
+          <Route index element={<Home />} />
+          
+        </Route>
+      </Routes>
+    </BrowserRouter>
+    <Header/>
     </>
   )
 }
